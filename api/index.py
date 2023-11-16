d50649f802acdf76bc7df4cf45875ae5fac27780
ion399 <shark.marine@msa.hinet.net>
2023-11-16 23:49:53 +0800

    Initial commit

---
+++ api/index.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
+	
+@app.route('/ion')
+def callback():
+    return 'Hello ion ...'
